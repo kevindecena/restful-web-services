714369cef08188c4db9aeccf19deda063a95cc2c
Kevin Decena <kcdecena.16@gmail.com>
2023-04-18 17:36:44 +0800

    initial commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -21,6 +22,11 @@ public class UserDaoService {
 
     public List<User> findAll(){
         return users;
+    }
+
+    public User findOne(int id){
+        Predicate<? super User> prediate = user -> user.getId().equals(id);
+        return users.stream().filter(prediate).findFirst().get();
     }
 
 }
