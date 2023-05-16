6e83680a23c193a211983dbdb346ed57e8d0b912
Kevin Decena <kcdecena.16@gmail.com>
2023-05-16 09:07:23 +0800

    versioning with URIs, Params,Request and Accept headers

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,25 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("PersonV2{");
+        sb.append("name=").append(name);
+        sb.append('}');
+        return sb.toString();
+    }
+}
