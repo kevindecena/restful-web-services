829f463b4c47cc4498b4ecac08649ed320b664a2
Kevin Decena <kcdecena.16@gmail.com>
2023-05-16 12:36:28 +0800

    connected to H2 Database

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,15 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
-    @Size(min = 2, message = "name should have atleast 2 characters")
+    @Size(min = 2, message = "name should have at least 2 characters")
     @JsonProperty("user_name")//customize names
     private String name;
     @Past(message = "Birthdate should be in the past")
